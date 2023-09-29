affef8154b60c70ed0da6223856880e0324ece9c
Aditya Raj Bhagat <adityabhagat900@gmail.com>
2023-09-30 00:51:00 +0530

    Pointers

--- Maths/Module_Arithmatic.cpp
+++ Maths/Module_Arithmatic.cpp
@@ -1,4 +1,26 @@
 /* formulas for module
 (a+b)%m  =a % m + b %m
-a % m -  b % m  =  
+a % m -  b % m  = (a-b) %m 
+a % m *  b % m  = (a*b) %m
+
+
+Fast Exponentation
 */
+
+#include <bits/stdc++.h>
+
+int modularExponentiation(int x, int n, int m) {
+	int res=1;
+
+	while(n>0){
+		if(n&1){
+			res = (1LL*(res) *(x)%m)%m;
+		}
+
+		x=(1LL*(x)%m * (x)%m)%m;
+		n=n>>1;
+	}
+	return res;
+
+
+}
