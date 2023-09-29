ee9bc692d301aa0ae99c65a3682f329bd66407e9
Aditya Raj Bhagat <adityabhagat900@gmail.com>
2023-09-29 23:04:34 +0530

    Maths

---
+++ Maths/PrimeNo.cpp
@@ -0,0 +1,54 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+
+
+// bool PrimeNo(int n){
+//     for(int i=2; i<n; i++){
+//         if(n%i==0)return false;
+        
+//     }
+
+//     return true;
+// }
+
+// int main(){
+//     int n;
+//     cout<<"Enter a number "<<endl;
+//     cin>>n;
+
+//     if(PrimeNo(n)){
+//         cout<<"It is a prime number"<<endl;
+//     }
+//     else{
+//         cout<<"it is not a prime number"<<endl;
+//     }
+    
+
+
+// }
+
+
+/******sieve of eratosthenes******/
+
+
+class Solution {
+public:
+    int countPrimes(int n) {
+        int count=0;
+        
+        vector<bool>prime(n+1, true);
+        prime[0]=prime[1]=false;
+        
+        for(int i=2; i<n;i++){
+            if(prime[i]){
+                count++;
+            
+            
+            for(int j= 2*i; j<n; j=j+i){
+                prime[j]=0;
+            } 
+            }
+        }
+        return count;
+    }
+};
